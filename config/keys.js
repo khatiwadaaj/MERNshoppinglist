fd807afda3279c23e124831b0614107e1d2b212a
Ajaya Khatiwada <khatiwadaajaya@gmail.com>
2018-08-26 14:21:49 +1000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://ajay:ajay1234@ds233452.mlab.com:33452/mern_shopping'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Item Model
+const Item = require ('../../models/Item');
+
+
+
+module.exports = router;
